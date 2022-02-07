76be79482e8edffd165c456481255fc2f79fa497
Arjun Ladha <tigerninja2234@gmail.com>
2022-02-07 16:25:14 +0530

    Made api fetching method

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+
+
+// middleware
+
+app.use(express.json())
+
+// routes
+app.get('/', (req, res) => {
+    res.send('Welcome to your Task Manager App.');
+});
+
+app.use('/api/v1/tasks', tasks)
+
+// app.get('/api/v1/tasks')       - get all the tasks
+// app.post('/api/v1/tasks')      - create a new task
+// app.get('/api/v1/tasks/:id')   - get single task
+// app.patch('api/v1/tasks/:id')  - update task
+// app.delete('api/v1/tasks/:id') - delete task
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, console.log(`Listening on port ${port}...`));
