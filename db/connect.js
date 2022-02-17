c4b56baf3463811d4ac3b4f87bf9b62a715bf1cc
Arjun Ladha <tigerninja2234@gmail.com>
2022-02-17 19:31:38 +0530

    Change the start color of the font from green to blue

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const connectDB = (url) =>{
-    console.log('%c Connected to database...', 'color: green; font-weight: bold;');
+    console.log('%c Connected to database...', 'color: blue; font-weight: bold;');
     return mongoose.connect(url)
 }
 
